416b15f99cf0394723e51f347d79b0a21e057e7f
Krystian <kszewczyk@inf.ug.edu.pl>
2016-01-21 01:25:26 +0100

    Dodano nowe funkcje w plikach ManagerImpl.java oraz Manager.java

--- src/main/java/com/service/Manager.java
+++ src/main/java/com/service/Manager.java
@@ -2,8 +2,13 @@ package com.service;
 
 import com.domain.Provider;
 
+import java.util.List;
+
 public interface Manager {
     //Provider
     Long addProvider(Provider provider);
+    void deleteProvider(Provider provider);
     Provider getProviderByNip(String providerNip);
+    List<Provider> getAllProviders();
+    Provider getProviderById(Long providerId);
 }
--- src/main/java/com/service/ManagerImpl.java
+++ src/main/java/com/service/ManagerImpl.java
@@ -1,10 +1,13 @@
 package com.service;
 
+import com.domain.Product;
 import com.domain.Provider;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Component
 @Transactional
@@ -30,5 +33,26 @@ public class ManagerImpl implements Manager {
     @Override
     public Provider getProviderByNip(String providerNip) {
         return (Provider) sessionFactory.getCurrentSession().getNamedQuery("provider.byNip").setString("providerNip", providerNip).uniqueResult();
+    }
+
+    @Override
+    public List<Provider> getAllProviders() {
+        return sessionFactory.getCurrentSession().getNamedQuery("provider.all").list();
+    }
+
+    @Override
+    public void deleteProvider(Provider provider) {
+        provider = (Provider) sessionFactory.getCurrentSession().get(Provider.class, provider.getProviderId());
+
+        for (Product product : provider.getProducts()) {
+            product.setProvider(null);
+            sessionFactory.getCurrentSession().update(product);
+        }
+        sessionFactory.getCurrentSession().delete(provider);
+    }
+
+    @Override
+    public Provider getProviderById(Long providerId) {
+        return (Provider) sessionFactory.getCurrentSession().get(Provider.class, providerId);
     }
 }
